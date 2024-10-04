65df48939612bb509ba9c499bc50fe345326ba77
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-10-04 10:53:07 -0300

    create in-memmory-users-repository

---
+++ src/app/repositories/in-memmory/in-memmory-users-repository.ts
@@ -0,0 +1,23 @@
+import { Prisma, User } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+import { randomUUID } from "crypto";
+
+export class InMemmoryUsersRepository implements UsersRepository {
+  public items: User[] = [];
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      cnpj: data.cnpj,
+      email: data.email,
+      password: data.password,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    this.items.push(user);
+
+    return user;
+  }
+}
