a9152c21003af5624a079283f6c37c84f8114203
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-10-06 11:12:46 -0300

    test get-list-questions

---
+++ src/app/use-cases/users/questions/get-list-questions.spec.ts
@@ -0,0 +1,29 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemmoryQuestionsRepository } from "../../../repositories/in-memmory/in-memmory-questions-repository";
+import { GetListQuestionUseCase } from "./get-list-questions";
+import { TitleLengthError } from "../../../errors/TitleLengthError";
+import { randomUUID } from "crypto";
+
+let questionsRepository: InMemmoryQuestionsRepository;
+let sut: GetListQuestionUseCase;
+
+describe("Create question test", () => {
+  beforeEach(() => {
+    questionsRepository = new InMemmoryQuestionsRepository();
+    sut = new GetListQuestionUseCase(questionsRepository);
+  });
+
+  it("should be able to get list questions", async () => {
+    await questionsRepository.create({
+      id: randomUUID(),
+      title: "Como foi a sua experiencia na loja?",
+      userId: "user-id",
+    });
+
+    const { questions } = await sut.execute({
+      userId: "user-id",
+    });
+
+    expect(questions).toHaveLength(1);
+  });
+});
