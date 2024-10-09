9224fbb4101c4f1e2274754ec34a2cbbb3237034
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-10-09 14:05:41 -0300

    cerate interceptors api and instance axios

---
+++ src/app/errors/AuthTokenError.ts
@@ -0,0 +1,6 @@
+export class AuthTokenError extends Error {
+    constructor() {
+      super("Auth token error");
+    }
+  }
+  
---
+++ src/app/lib/axios/api.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosError } from "axios";
+import { parseCookies } from "nookies";
+import { AuthTokenError } from "../../errors/AuthtokenError";
+
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: "http://localhost:3000",
+    headers: {
+      Authorization: `Bearer ${cookies["@auth-itattoo:token"]}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      // Verifica se o status é 401 (não autorizado)
+      if (error.response?.status === 401) {
+        if (typeof window !== "undefined") {
+        //   signOut(); // Desloga o usuário no client-side
+        } else {
+          return Promise.reject(new AuthTokenError()); // Lança erro customizado no server-side
+        }
+      }
+
+      return Promise.reject(error); // Retorna o erro para ser tratado em outro lugar, se necessário
+    },
+  );
+
+  return api;
+}
